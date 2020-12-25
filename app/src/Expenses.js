0291d26482dc482d52d635a7f41054ca27eaad9d
Moe Bachrouch <moe.bachrouch@gmail.com>
2020-12-25 17:45:11 -0500

    started expenses page

--- app/src/Expenses.js
+++ app/src/Expenses.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import AppNav from './AppNav';
+import DatePicker from 'react-datepicker';
+import './App.css';
+import "react-datepicker/dist/react-datepicker.css";
+import {Container, Input, Button, Label, Form, FormGroup} from 'reactstrap';
+import {Link} from 'react-router-dom';
 
 class Expenses extends Component {
     state = {  }
@@ -7,7 +12,35 @@ class Expenses extends Component {
         return (
             <div>
                 <AppNav/> 
-                <h2>Expenses</h2> 
+                <Container>
+
+                   <Form>
+                        <FormGroup>
+                            <label for = "title">Title</label>   
+                            <input type = "text" name = "title" id = "title" onChange={this.handleChange}/>
+                        </FormGroup>  
+
+                       <FormGroup>
+                            <label for = "category">Category</label>   
+                            <input type = "text" name = "category" id = "category" onChange={this.handleChange}/>
+                        </FormGroup> 
+
+                        <FormGroup>
+                            <label for = "expenseDate">Expense Date</label>
+                            <DatePicker selected = {this.state.date} onChange = {this.handleChange}/> 
+                        </FormGroup>
+
+                        <FormGroup>
+                            <label for = "location">Location</label>   
+                            <input type = "text" name = "location" id = "location" onChange={this.handleChange}/>
+                        </FormGroup>   
+
+                        <FormGroup>
+                            <Button color = "primary" type = "submit">Save</Button> {' '}
+                            <Button color = "secondary" tag={Link} to="/categories">Cancel</Button>"
+                        </FormGroup>   
+                    </Form> 
+                </Container>
             </div>
             
         );
